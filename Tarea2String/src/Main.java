74811cdc6841d1076735a4d963c3876fb3fc52bd
MartinRamirez06 <125822553+MartinRamirez06@users.noreply.github.com>
2023-02-21 00:08:00 -0600

    Add files via upload

---
+++ Tarea2String/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder Nom=new StringBuilder();
+        Nom.append("Nombre:Martin\n");
+        Nom.append("Profesion:Estudiante\n");
+        Nom.append("Nacionalidad:Mexico");
+        System.out.println(Nom);
+    }
+}
